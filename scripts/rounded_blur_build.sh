4b28a8845d0ee03959e13cbfa8c4fe70edfd7f0d
MeguMario <ttrungpt2@gmail.com>
2026-04-17 11:45:50 +0700

    Overhaul script

--- scripts/rounded_blur_build.sh
+++ scripts/rounded_blur_build.sh
@@ -1,9 +1,124 @@
 #!/bin/bash
 
+check_env(){
+	OS_ID_TYPE=$(cat /etc/os-release | grep -m 1 -o -P '(?<=ID=).*')
+	OS_LIKE_ID_TYPE=$(cat /etc/os-release | grep -m 1 -o -P '(?<=ID_LIKE=).*' || true)
+
+	if [[ "$OS_ID_TYPE" = "arch" ]] || [[ "$OS_LIKE_ID_TYPE" = "arch" ]]; then
+		if [[ $i = "y" ]] && [[ $u = "n" ]]; then		
+			echo "--------------------------------------------------------"
+			echo "Please do not use this script to install gnome-rounded-blur on Arch Linux"
+			echo "To install this library on Arch, please do so via the AUR"
+			echo "https://aur.archlinux.org/packages/gnome-rounded-blur"
+			echo "--------------------------------------------------------"
+		elif [[ $i = "n" ]] && [[ $u = "y" ]]; then	
+			echo "--------------------------------------------------------"
+			echo "Please do not use this script to uninstall gnome-rounded-blur on Arch Linux"
+			echo "To uninstall this library on Arch, please use the following command"
+			echo "< sudo pacman -R gnome-rounded-blur >"
+			echo "--------------------------------------------------------"
+		fi
+		OS_TYPE="arch"
+		sleep 5
+		exit 1
+	elif [[ "$OS_ID_TYPE" = "debian" ]] || [[ "$OS_LIKE_ID_TYPE" = "debian" ]]; then
+		OS_TYPE="debian"
+	elif [[ "$OS_ID_TYPE" = "fedora" ]] || [[ "$OS_LIKE_ID_TYPE" = "fedora" ]]; then
+		OS_TYPE="fedora"
+	else
+		OS_TYPE="unknown"
+	fi
+}
+
+install_git(){
+	if ! command -v git >/dev/null 2>&1
+	then
+		if [[ "$OS_TYPE"="debian" ]]; then
+			echo "--------------------------------------------------------"
+			echo "Installing git"
+			echo "--------------------------------------------------------"
+			sudo apt install git 
+		elif [[ "$OS_TYPE"="fedora" ]]; then
+			echo "--------------------------------------------------------"
+			echo "Installing git"
+			echo "--------------------------------------------------------"
+			sudo dnf -y install git
+		else
+			echo "--------------------------------------------------------"
+			echo "Please manually install git using your distro's package manager"
+			echo "--------------------------------------------------------"
+			sleep 5
+			exit 1
+		fi
+	fi
+}
+
+install_dep(){
+	if [[ "$OS_TYPE"="debian" ]]; then
+		echo "--------------------------------------------------------"
+		echo "Installing dependency"
+		echo "--------------------------------------------------------"
+		sudo apt install libglib2.0-dev build-essential libmutter-$DIFF_VALUE_2-dev gobject-introspection meson
+	elif [[ "$OS_TYPE"="fedora" ]]; then
+		echo "--------------------------------------------------------"
+		echo "Installing dependency"
+		echo "--------------------------------------------------------"
+		sudo dnf -y install glib2-devel @c-development meson mutter-devel gobject-introspection
+	else
+		echo "--------------------------------------------------------"
+		echo "Please manually install the equivalent of libglib2.0-dev build-essential libmutter-$DIFF_VALUE_2-dev gobject-introspection meson on your computer"
+		echo "The setup will still proceed and fail if you don't have those installed"
+		echo "--------------------------------------------------------"
+		sleep 5
+	fi
+}
+
 install_lib(){
+	prep_stage
+		
+	echo "--------------------------------------------------------"
+	echo "Building the library"
+	echo "--------------------------------------------------------"
+	meson setup build;
+	meson compile -C build;
+	
+	# meson install the library in the wrong directory, we'll do that ourselves
+	echo "--------------------------------------------------------"
+	echo "Installing the library"
+	echo "--------------------------------------------------------"
+	meson install -C build --destdir "$dest_dir"
+	sudo install -D -m 655 -o root -t /usr/ ./build/binary/usr/local/* 
+	
+	echo "--------------------------------------------------------"
+	echo "For the changes to apply, please log out and then log back in."
+	echo "--------------------------------------------------------"
+}
+
+uninstall_lib(){
+	check_env
+	
+	if [[ "$OS_TYPE" = "debian" ]] || [[ "$OS_TYPE" = "fedora" ]]; then
+		echo "--------------------------------------------------------"
+		echo "Uninstalling"
+		echo "--------------------------------------------------------"
+		sudo rm -rf /usr/include/blur-effect-1.0
+		sudo rm /usr/lib/girepository-1.0/Blur-1.0.typelib /usr/lib/pkgconfig/blur-effect-1.0.pc /usr/lib/libblur-effect-1.0.so /usr/lib/libblur-effect-1.0.so.1 /usr/lib/libblur-effect-1.0.so.1.0.0 /usr/share/gir-1.0/Blur-1.0.gir
+		echo "--------------------------------------------------------"
+		echo "For the changes to apply, please log out and then log back in."
+		echo "--------------------------------------------------------"
+	fi
+}
+
+prep_stage(){
 	REPO="https://github.com/kancko/gnome-rounded-blur"
 	dest_dir="./binary"
 	build_dir="/tmp"
+	
+	# Check for current environment before doing anything
+	check_env
+	
+	# Install git first before doing anything else
+	install_git
 	
 	echo "--------------------------------------------------------"
 	echo "Cloning repo"
@@ -37,87 +152,6 @@ install_lib(){
 	fi
 	
 	install_dep;
-	
-	echo "--------------------------------------------------------"
-	echo "Building the library"
-	echo "--------------------------------------------------------"
-	meson setup build;
-	meson compile -C build;
-	
-	# meson install the library in the wrong directory, we'll do that ourselves
-	echo "--------------------------------------------------------"
-	echo "Installing the library"
-	echo "--------------------------------------------------------"
-	meson install -C build --destdir "$dest_dir"
-	sudo cp -rf ./build/binary/usr/local/* /usr/
-	
-	echo "--------------------------------------------------------"
-	echo "For the changes to apply, please log out and then log back in."
-	echo "--------------------------------------------------------"
-}
-
-uninstall_lib(){
-	OS_ID_TYPE=$(cat /etc/os-release | grep -m 1 -o -P '(?<=ID=).*')
-	OS_LIKE_ID_TYPE=$(cat /etc/os-release | grep -m 1 -o -P '(?<=ID_LIKE=).*' || true)
-	
-	if [[ "$OS_ID_TYPE" = "arch" ]] || [[ "$OS_LIKE_ID_TYPE" = "arch" ]]; then
-	 	echo "--------------------------------------------------------"
-		echo "Please do not use this script to uninstall the library if you installed it via the AUR,"
-		echo "use pacman -R gnome-rounded-blur instead"
-		echo "--------------------------------------------------------"
-		sleep 5
-		exit 1
-	else
-		echo "--------------------------------------------------------"
-		echo "Uninstalling"
-		echo "--------------------------------------------------------"
-		sudo rm -rf /usr/include/blur-effect-1.0
-		sudo rm /usr/lib/girepository-1.0/Blur-1.0.typelib /usr/lib/pkgconfig/blur-effect-1.0.pc /usr/lib/libblur-effect-1.0.so /usr/lib/libblur-effect-1.0.so.1 /usr/lib/libblur-effect-1.0.so.1.0.0 /usr/share/gir-1.0/Blur-1.0.gir
-	fi
-
-	echo "--------------------------------------------------------"
-	echo "For the changes to apply, please log out and then log back in."
-	echo "--------------------------------------------------------"
-}
-
-# More safety, by turning some bugs into errors.
-set -o errexit -o pipefail -o noclobber -o nounset
-
-# ignore errexit with `&& true`
-getopt --test > /dev/null && true
-if [[ $? -ne 4 ]]; then
-    echo 'I’m sorry, `getopt --test` failed in this environment.'
-    exit 1
-fi
-
-install_dep(){
-	OS_ID_TYPE=$(cat /etc/os-release | grep -m 1 -o -P '(?<=ID=).*')
-	OS_LIKE_ID_TYPE=$(cat /etc/os-release | grep -m 1 -o -P '(?<=ID_LIKE=).*' || true)
-
-	if [[ "$OS_ID_TYPE" = "arch" ]] || [[ "$OS_LIKE_ID_TYPE" = "arch" ]]; then
-	 	echo "--------------------------------------------------------"
-		echo "Please install this library via the AUR"
-		echo "https://aur.archlinux.org/packages/gnome-rounded-blur"
-		echo "--------------------------------------------------------"
-		sleep 5
-		exit 1
-	elif [[ "$OS_ID_TYPE" = "debian" ]] || [[ "$OS_LIKE_ID_TYPE" = "debian" ]]; then
-		echo "--------------------------------------------------------"
-		echo "Installing dependency"
-		echo "--------------------------------------------------------"
-		sudo apt install git libglib2.0-dev build-essential libmutter-$DIFF_VALUE_2-dev gobject-introspection meson
-	elif [[ "$OS_ID_TYPE" = "fedora" ]] || [[ "$OS_LIKE_ID_TYPE" = "fedora" ]]; then
-		echo "--------------------------------------------------------"
-		echo "Installing dependency"
-		echo "--------------------------------------------------------"
-		sudo dnf -y install git glib2-devel @c-development meson mutter-devel gobject-introspection
-	else
-		echo "--------------------------------------------------------"
-		echo "Please manually install the equivalent of libglib2.0-dev build-essential libmutter-$DIFF_VALUE_2-dev gobject-introspection meson on your computer"
-		echo "The setup will still proceed and fail if you don't have those installed"
-		echo "--------------------------------------------------------"
-		sleep 5
-	fi
 }
 
 help_doc(){
@@ -128,6 +162,17 @@ help_doc(){
 	echo "-u			Uninstall the library"
 	echo "-h			Help"
 }
+
+
+# More safety, by turning some bugs into errors.
+set -o errexit -o pipefail -o noclobber -o nounset
+
+# ignore errexit with `&& true`
+getopt --test > /dev/null && true
+if [[ $? -ne 4 ]]; then
+    echo 'I’m sorry, `getopt --test` failed in this environment.'
+    exit 1
+fi
 
 LONGOPTS=install,uninstall,help
 OPTIONS=iuh
@@ -151,7 +196,7 @@ while true; do
             break
             ;;
 				-u|--uninstall)
-						k=y
+						u=y
             uninstall_lib
             shift
             break
